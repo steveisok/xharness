2b4b1f231735b51a792ae3a84d3e984092d0978c
Premek Vysoky <premek.vysoky@microsoft.com>
2020-04-28 10:41:16 +0200

    Try the System.Numerics.Vectors.Tests.app

--- tests/integration-tests/osx-helix-payload.sh
+++ tests/integration-tests/osx-helix-payload.sh
@@ -11,8 +11,17 @@ mkdir $here/workdir
 # TODO - Call: dotnet xharness ios package ...
 # For now we download it:
 # curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/DummyTestProject.app.zip?sp=r&st=2020-04-22T09:38:37Z&se=2020-10-22T17:38:37Z&spr=https&sv=2019-10-10&sr=b&sig=h54BDrAf%2BenDyPbFhbCjj%2FiBLFMS4taHJ1dlrDDTxSs%3D" --output $here/app.zip
+#app_name='DummyTestProject.app'
+
 # curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/Program.Tests.app.zip?sp=r&st=2020-04-23T18:57:58Z&se=2020-06-19T02:57:58Z&spr=https&sv=2019-10-10&sr=b&sig=BRVEKgPp2WMpJNSFpNssMg4oyMX8VbcsOwpRdgqbujY%3D" --output $here/app.zip
-curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/com.xamarin.bcltests.BCL%20tests%20group%203.zip?sp=r&st=2020-04-24T14:50:45Z&se=2021-04-24T22:50:45Z&spr=https&sv=2019-10-10&sr=b&sig=vQglB83jgTQoSfI42qrjgzPzIDD03X2T2vD%2BicNtuQE%3D" --output $here/app.zip
+#app_name='Program.Tests.app'
+
+# curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/com.xamarin.bcltests.BCL%20tests%20group%203.zip?sp=r&st=2020-04-24T14:50:45Z&se=2021-04-24T22:50:45Z&spr=https&sv=2019-10-10&sr=b&sig=vQglB83jgTQoSfI42qrjgzPzIDD03X2T2vD%2BicNtuQE%3D" --output $here/app.zip
+#app_name='com.xamarin.bcltests.BCL tests group 3.app'
+
+curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/System.Numerics.Vectors.Tests.app.zip?sp=r&st=2020-04-28T08:40:27Z&se=2020-10-08T16:40:27Z&spr=https&sv=2019-10-10&sr=b&sig=EZ2n5i7ea6nccjStWMLmgzoMbIDpA0MREGVK%2Bg7c050%3D" --output $here/app.zip
+app_name='System.Numerics.Vectors.Tests.app'
+
 tar -xzf app.zip
 
 mkdir $here/tools
@@ -28,7 +37,7 @@ export XHARNESS_LOG_WITH_TIMESTAMPS=true
 dotnet xharness ios test \
     --working-directory="$here/workdir" \
     --output-directory="$HELIX_WORKITEM_UPLOAD_ROOT" \
-    --app="$here/com.xamarin.bcltests.BCL tests group 3.app" \
+    --app="$here/$app_name" \
     --targets=ios-simulator-64 \
     --timeout=400 \
     --launch-timeout=180
