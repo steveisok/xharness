13067bf49e677188e5fd34d8cc5069617c226d43
Premek Vysoky <premek.vysoky@microsoft.com>
2020-04-28 10:35:28 +0200

    Add integration tests stage to our pipeline

---
+++ tests/integration-tests/osx-helix-payload.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo '################### Xharness start ###################'
+
+set -ex
+
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir $here/workdir
+
+# TODO - Call: dotnet xharness ios package ...
+# For now we download it:
+# curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/DummyTestProject.app.zip?sp=r&st=2020-04-22T09:38:37Z&se=2020-10-22T17:38:37Z&spr=https&sv=2019-10-10&sr=b&sig=h54BDrAf%2BenDyPbFhbCjj%2FiBLFMS4taHJ1dlrDDTxSs%3D" --output $here/app.zip
+# curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/Program.Tests.app.zip?sp=r&st=2020-04-23T18:57:58Z&se=2020-06-19T02:57:58Z&spr=https&sv=2019-10-10&sr=b&sig=BRVEKgPp2WMpJNSFpNssMg4oyMX8VbcsOwpRdgqbujY%3D" --output $here/app.zip
+curl "https://prvysokyexperiment.blob.core.windows.net/ios-apps/com.xamarin.bcltests.BCL%20tests%20group%203.zip?sp=r&st=2020-04-24T14:50:45Z&se=2021-04-24T22:50:45Z&spr=https&sv=2019-10-10&sr=b&sig=vQglB83jgTQoSfI42qrjgzPzIDD03X2T2vD%2BicNtuQE%3D" --output $here/app.zip
+tar -xzf app.zip
+
+mkdir $here/tools
+cd $here/tools
+
+dotnet new tool-manifest
+
+dotnet tool install --version 1.0.0-ci --add-source .. Microsoft.DotNet.XHarness.CLI
+
+export XHARNESS_DISABLE_COLORED_OUTPUT=true
+export XHARNESS_LOG_WITH_TIMESTAMPS=true
+
+dotnet xharness ios test \
+    --working-directory="$here/workdir" \
+    --output-directory="$HELIX_WORKITEM_UPLOAD_ROOT" \
+    --app="$here/com.xamarin.bcltests.BCL tests group 3.app" \
+    --targets=ios-simulator-64 \
+    --timeout=400 \
+    --launch-timeout=180
+
+echo '#################### Xharness end ####################'
